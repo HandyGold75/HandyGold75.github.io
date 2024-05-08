2a24ee47d9d57e594cf184d1d003a31536815c8c
HandyGold75 <IZO@HandyGold75.com>
2024-05-08 23:45:56 +0200

    09-May-24

--- Pages/Home.go
+++ Pages/Home.go
@@ -13,7 +13,7 @@ func PageHome() {
 		Tag:        "p",
 		Attributes: map[string]string{"id": "someText"},
 		Styles:     map[string]string{"color": "#55F"},
-		Inner:      "Moving towards GO wasm instead of Python wasm<br><br><br><br><br><br><br><br><br><br><br><br><br>For reasons...<br>Old site should still be available at " + HTML.HTML{Attributes: map[string]string{"href": "./python"}, Inner: "./python"}.ApplyTemplate(HTML.HTML_Link).String(),
+		Inner:      "Moving towards GO wasm instead of Python wasm<br><br>For reasons...<br>Old site should still be available at " + HTML.HTML{Attributes: map[string]string{"href": "./python"}, Inner: "./python"}.ApplyTemplate(HTML.HTML_Link).String(),
 	}.String()
 
 	mp, err := DOM.GetElement("mainpage")
