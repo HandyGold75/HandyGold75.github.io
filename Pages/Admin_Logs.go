6ff4569fe32d943fbb1724dca207b5308e7fe2cb
HandyGold75 <IZO@HandyGold75.com>
2024-07-04 17:39:21 +0200

    04-Jul-24

--- Pages/Admin_Logs.go
+++ Pages/Admin_Logs.go
@@ -17,6 +17,11 @@ import (
 	"time"
 )
 
+var (
+	availableLogs = map[string][]string{}
+	isBusy        = false
+)
+
 func LogListCallback(res string, resBytes []byte, resErr error) {
 	if resErr == WebKit.ErrWebKit.HTTPUnauthorized || resErr == WebKit.ErrWebKit.HTTPNoServerSpecified {
 		OnLoginSuccessCallback = func() { JS.Async(func() { ForcePage("Admin:Logs") }) }
@@ -26,15 +31,19 @@ func LogListCallback(res string, resBytes []byte, resErr error) {
 		return
 	}
 
-	logs := map[string][]string{}
-	err := json.Unmarshal(resBytes, &logs)
+	availableLogs = map[string][]string{}
+	err := json.Unmarshal(resBytes, &availableLogs)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
 	logTypes := HTML.HTML{Tag: "p", Styles: map[string]string{"margin": "auto"}}.String()
-	for k := range logs {
+	for k := range availableLogs {
+		if len(availableLogs[k]) == 0 {
+			continue
+		}
+
 		logTypes += HTML.HTML{Tag: "button",
 			Attributes: map[string]string{"class": "dark medium logs_types_buttons"},
 			Inner:      k,
@@ -55,45 +64,153 @@ func LogListCallback(res string, resBytes []byte, resErr error) {
 		return
 	}
 
-	els.StylesSet("min-width", strconv.Itoa(min(10, 100/len(logs)))+"%")
+	els.StylesSet("min-width", strconv.Itoa(min(10, 100/len(availableLogs)))+"%")
 	els.EventsAdd("click", func(el js.Value, evs []js.Value) {
-		selectedLog := el.Get("innerHTML").String()
-		log, ok := logs[selectedLog]
-		if !ok {
-			fmt.Println(err)
-			return
-		}
-
-		logDates := HTML.HTML{Tag: "p", Styles: map[string]string{"margin": "auto"}}.String()
-		for _, v := range log {
-			logDates += HTML.HTML{Tag: "button",
-				Attributes: map[string]string{"class": "dark small logs_types_dates"},
-				Inner:      v,
-			}.String()
-		}
-		logDates += HTML.HTML{Tag: "p", Styles: map[string]string{"margin": "auto"}}.String()
-
 		elDates, err := DOM.GetElement("logs_dates")
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		elDates.InnerSet(logDates)
 
-		els, err := DOM.GetElements("logs_types_dates")
-		if err != nil {
-			fmt.Println(err)
-			return
+		delay := 0
+		if elDates.StyleGet("max-height") != "0px" {
+			elDates.StyleSet("max-height", "0px")
+			delay = 250
 		}
 
-		els.StylesSet("min-width", strconv.Itoa(min(5, 100/len(logs)))+"%")
-		els.EventsAdd("click", func(el js.Value, evs []js.Value) {
-			HTTP.Send(getLogCallback, "logs", "get", selectedLog, el.Get("innerHTML").String())
+		selectedLog := el.Get("innerHTML").String()
+		JS.AfterDelay(delay, func() {
+			showLogDates(selectedLog)
 		})
 	})
 }
 
+func showLogDates(selected string) {
+	log, ok := availableLogs[selected]
+	if !ok {
+		fmt.Println("log \"" + selected + "\" not available!")
+		return
+	}
+
+	logDates := HTML.HTML{Tag: "p", Styles: map[string]string{"margin": "auto"}}.String()
+	for _, v := range log {
+		logDates += HTML.HTML{Tag: "button",
+			Attributes: map[string]string{"class": "dark small logs_types_dates"},
+			Inner:      v,
+		}.String()
+	}
+	logDates += HTML.HTML{Tag: "p", Styles: map[string]string{"margin": "auto"}}.String()
+
+	elDates, err := DOM.GetElement("logs_dates")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	elDates.InnerSet(logDates)
+	elDates.StyleSet("max-height", "40px")
+
+	els, err := DOM.GetElements("logs_types_dates")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	els.StylesSet("min-width", strconv.Itoa(min(5, 100/len(availableLogs)))+"%")
+	els.EventsAdd("click", func(el js.Value, evs []js.Value) {
+		HTTP.Send(getLogCallback, "logs", "get", selected, el.Get("innerHTML").String())
+	})
+}
+
 func getLogCallback(res string, resBytes []byte, resErr error) {
+	log := []string{}
+	err := json.Unmarshal(resBytes, &log)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	lines := strings.Split(strings.Join(log, ""), "<EOR>\n")
+	slices.Reverse(lines)
+
+	showLogContent(lines)
+}
+
+func showLogContent(lines []string) {
+	if isBusy {
+		return
+	}
+	isBusy = true
+
+	el, err := DOM.GetElement("logs_out")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	el.InnerSet("")
+
+	for i, line := range lines {
+		if len(line) <= 0 {
+			continue
+		}
+
+		cols := ""
+		lineSplit := strings.Split(line, "<SEP>")
+		for i, col := range lineSplit {
+			if i == 0 {
+				t, err := time.Parse(time.RFC3339Nano, col)
+				if err == nil {
+					col = t.Format(time.DateTime)
+				}
+			}
+
+			if i == len(lineSplit)-1 {
+				cols += HTML.HTML{Tag: "p",
+					Styles: map[string]string{
+						"scrollbar-width": "thin",
+						"scrollbar-color": "transparent transparent",
+						"overflow":        "scroll",
+					},
+					Inner: col,
+				}.String()
+
+				continue
+			}
+
+			cols += HTML.HTML{Tag: "p",
+				Styles: map[string]string{
+					"min-width":       "10%",
+					"border-right":    "2px dashed #151515",
+					"border-radius":   "0px",
+					"scrollbar-width": "thin",
+					"scrollbar-color": "transparent transparent",
+					"overflow":        "scroll",
+				},
+				Inner: col,
+			}.String()
+		}
+
+		row := HTML.HTML{Tag: "div",
+			Styles: map[string]string{
+				"display":       "flex",
+				"padding":       "0px",
+				"margin-bottom": "-2px",
+				"background":    "#202020",
+				"border":        "2px solid #151515",
+				"border-radius": "0px",
+				"font-size":     "75%",
+			},
+			Inner: cols,
+		}.String()
+
+		JS.AfterDelay(i*5, func() {
+			el.InnerAddSurfix(row)
+		})
+	}
+
+	JS.AfterDelay((len(lines)-1)*5, func() { isBusy = false })
+}
+
+func getLogCallbackOld(res string, resBytes []byte, resErr error) {
 	log := []string{}
 	err := json.Unmarshal(resBytes, &log)
 	if err != nil {
@@ -188,15 +305,16 @@ func PageAdminLogs() {
 	}.String()
 
 	dates := HTML.HTML{Tag: "div",
-
 		Attributes: map[string]string{"id": "logs_dates"},
 		Styles: map[string]string{"display": "flex",
 			"width":         "95%",
+			"max-height":    "0px",
 			"background":    "#202020",
 			"border-left":   "2px solid #111",
 			"border-right":  "2px solid #111",
 			"border-bottom": "2px solid #111",
-			"border-radius": "0px 0px 10px 10px"},
+			"border-radius": "0px 0px 10px 10px",
+			"transition":    "max-height 0.25s"},
 	}.String()
 
 	out := HTML.HTML{Tag: "div",
