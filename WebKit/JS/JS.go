1bdfa5b71a7d0d97f1047ad6a19f1b402501c8ce
HandyGold75 <Ian.AnMaZo@gmail.com>
2024-08-01 09:37:01 +0200

    01-Aug-24

--- WebKit/JS/JS.go
+++ WebKit/JS/JS.go
@@ -102,12 +102,12 @@ func OnResizeDelete(key string) {
 }
 
 func Download(fileName string, data []byte) error {
-	elBody, err := DOM.GetElement("body")
+	el, err := DOM.GetElement("body")
 	if err != nil {
 		return err
 	}
 
-	elBody.InnerAddSurfix(HTML.HTML{Tag: "a",
+	el.InnerAddSurfix(HTML.HTML{Tag: "a",
 		Attributes: map[string]string{
 			"id":       fileName + "_download",
 			"href":     "data:text/json;charset=utf-8," + UriFriendlyfy(string(data)),
@@ -115,13 +115,143 @@ func Download(fileName string, data []byte) error {
 		Styles: map[string]string{"display": "none"},
 	}.String())
 
-	elDl, err := DOM.GetElement(fileName + "_download")
+	el, err = DOM.GetElement(fileName + "_download")
 	if err != nil {
 		return err
 	}
 
-	elDl.Call("click")
-	elDl.Remove()
+	el.Call("click")
+	el.Remove()
+
+	return nil
+}
+
+func ensurePopupDiv(title string, txt string, buttons string) error {
+	header := HTML.HTML{Tag: "h1", Inner: "Alert - " + title}.String()
+	text := HTML.HTML{Tag: "p", Inner: txt}.String()
+	btnDiv := HTML.HTML{Tag: "div",
+		Styles: map[string]string{"display": "flex", "margin": "10px 0px 0px 0px"},
+		Inner:  buttons,
+	}.String()
+
+	div := HTML.HTML{Tag: "div",
+		Styles: map[string]string{
+			"width":     "50%",
+			"min-width": "500px",
+			"max-width": "1000px",
+			"margin":    "50px auto",
+			"padding":   "10px",
+			"border":    "2px solid #55F",
+		},
+		Inner: header + text + btnDiv,
+	}.String()
+
+	popDiv := HTML.HTML{Tag: "div",
+		Attributes: map[string]string{"id": "popup"},
+		Styles: map[string]string{
+			"z-index":    "10000",
+			"position":   "absolute",
+			"top":        "0px",
+			"left":       "0px",
+			"width":      "100vw",
+			"height":     "100vh",
+			"margin":     "0px",
+			"padding":    "0px",
+			"background": "rgba(0, 0, 0, 0.5)",
+			"opacity":    "0",
+			"transition": "opacity 0.25s",
+		},
+		Inner: div,
+	}.String()
+
+	el, err := DOM.GetElement("body")
+	if err != nil {
+		return err
+	}
+	el.InnerAddSurfix(popDiv)
+
+	AfterDelay(10, func() {
+		el, err := DOM.GetElement("popup")
+		if err != nil {
+			return
+		}
+		el.StyleSet("opacity", "1")
+	})
+
+	return nil
+}
+
+func PopupAlert(title string, txt string, callback func()) error {
+	spacer := HTML.HTML{Tag: "div"}.String()
+	button := HTML.HTML{
+		Tag:        "button",
+		Attributes: map[string]string{"id": "popup_continue", "class": "dark medium"},
+		Inner:      "Continue",
+	}.String()
+
+	if err := ensurePopupDiv(title, txt, spacer+button+spacer); err != nil {
+		return err
+	}
+
+	el, err := DOM.GetElement("popup_continue")
+	if err != nil {
+		return err
+	}
+	el.EventAdd("click", func(el js.Value, evs []js.Value) {
+		elPop, err := DOM.GetElement("popup")
+		if err != nil {
+			Alert(err.Error())
+			return
+		}
+		elPop.StyleSet("opacity", "0")
+
+		AfterDelay(250, func() {
+			elPop.Remove()
+			callback()
+		})
+	})
+
+	return nil
+}
+
+func PopupConfirm(title string, txt string, falseText string, trueText string, callback func(bool)) error {
+	spacer := HTML.HTML{Tag: "div"}.String()
+	btnTrue := HTML.HTML{
+		Tag:        "button",
+		Attributes: map[string]string{"id": "popup_" + trueText, "class": "dark medium popup_buttons"},
+		Styles:     map[string]string{"min-width": "10%"},
+		Inner:      trueText,
+	}.String()
+	btnFalse := HTML.HTML{
+		Tag:        "button",
+		Attributes: map[string]string{"id": "popup_" + falseText, "class": "dark medium popup_buttons"},
+		Styles:     map[string]string{"min-width": "10%"},
+		Inner:      falseText,
+	}.String()
+
+	if err := ensurePopupDiv(title, txt, spacer+btnFalse+spacer+btnTrue+spacer); err != nil {
+		return err
+	}
+
+	els, err := DOM.GetElements("popup_buttons")
+	if err != nil {
+		return err
+	}
+	els.EventsAdd("click", func(el js.Value, evs []js.Value) {
+		elPop, err := DOM.GetElement("popup")
+		if err != nil {
+			Alert(err.Error())
+			return
+		}
+		elPop.StyleSet("opacity", "0")
+
+		value := el.Get("id").String() == "popup_"+trueText
+
+		AfterDelay(250, func() {
+			elPop.Remove()
+			callback(value)
+		})
+	})
 
 	return nil
 }
