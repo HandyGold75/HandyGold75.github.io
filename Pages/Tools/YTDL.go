4f70e02da36e373ef32bc995b2fdd09d794c5a77
HandyGold75 <IZO@HandyGold75.com>
2024-08-12 15:58:50 +0200

    12-Aug-24

--- Pages/Tools/YTDL.go
+++ Pages/Tools/YTDL.go
@@ -5,19 +5,49 @@ package Tools
 import (
 	"HandyGold75/WebKit/DOM"
 	"HandyGold75/WebKit/HTML"
+	"HandyGold75/WebKit/HTTP"
 	"HandyGold75/WebKit/JS"
 )
 
-func PageYTDL(forcePage func(string), setLoginSuccessCallback func(func())) {
-	ForcePage = forcePage
-	SetLoginSuccessCallback = setLoginSuccessCallback
+func accessCallbackYTDL(hasAccess bool, err error) {
+	if HTTP.IsAuthError(err) {
+		SetLoginSuccessCallback(func() { JS.Async(func() { ForcePage("Tools:YTDL") }) })
+		return
+	} else if err != nil {
+		JS.Alert(err.Error())
+		return
+	}
 
+	if !hasAccess {
+		JS.Alert("unauthorized")
+		return
+	}
+
+	showYTDL()
+}
+
+func showYTDL() {
 	header := HTML.HTML{Tag: "h1", Inner: "YTDL"}.String()
+
+	body := HTML.HTML{Tag: "div"}.String()
 
 	mp, err := DOM.GetElement("mainpage")
 	if err != nil {
 		JS.Alert(err.Error())
 		return
 	}
-	mp.InnerSet(header)
+	mp.InnerSet(header + body)
+}
+
+func PageYTDL(forcePage func(string), setLoginSuccessCallback func(func())) {
+	ForcePage = forcePage
+	SetLoginSuccessCallback = setLoginSuccessCallback
+
+	if !HTTP.IsMaybeAuthenticated() {
+		SetLoginSuccessCallback(func() { JS.Async(func() { ForcePage("Tools:YTDL") }) })
+		JS.Async(func() { ForcePage("Login") })
+		return
+	}
+
+	HTTP.HasAccessTo(accessCallbackYTDL, "ytdl")
 }
