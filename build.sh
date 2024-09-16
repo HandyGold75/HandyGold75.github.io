ce2380f2d5f5b8ff04b6a85902a9846b2954cf49
HandyGold75 <Ian.AnMaZo@gmail.com>
2024-09-16 15:27:30 +0200

    16-Sep-24

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@
 go mod edit -go `go version | { read _ _ v _; echo ${v#go}; }`
 go mod tidy
 go get -u ./
+
 GOOS=js GOARCH=wasm go build -o ./docs/wasm/main.wasm  && echo -e "\033[32mBuild: ./docs/wasm/main.wasm\033[0m" || echo -e "\033[31mFailed: ./docs/wasm/main.wasm\033[0m"
 
