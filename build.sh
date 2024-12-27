5380270218d5049e9106c7c8cb644a83da5624da
HandyGold75 <Ian.AnMaZo@gmail.com>
2024-12-27 10:51:30 +0100

    27-Dec-24

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-go mod edit -go "$(go version | { read -r _ _ v _; echo "${v#go}"; })"
-go mod tidy
-go get -u ./
+update(){
+    go mod edit -go "$(go version | { read -r _ _ v _; echo "${v#go}"; })" || { echo -e "\033[31mFailed: $1.*\033[0m" ; return 1; }
+    go mod tidy || { echo -e "\033[31mFailed: $1.*\033[0m" ; return 1; }
+    go get -u ./ || { echo -e "\033[31mFailed: $1.*\033[0m" ; return 1; }
+}
+
+buildwasm(){
+    ( env GOOS=js GOARCH=wasm go build -o "$1.wasm" . && echo -e "\033[32mBuild: $1.wasm\033[0m" ) || { echo -e "\033[31mFailed: $1.wasm\033[0m" ; return 1; }
+}
 
 file="./docs/wasm/main"
 
-env GOOS=js GOARCH=wasm go build -o "$file.wasm" . && echo -e "\033[32mBuild: $file.wasm\033[0m" || echo -e "\033[31mFailed: $file.wasm\033[0m"
-
+update "$file" && buildwasm "$file"
