8c522e1bceda9f07657900cf768b5ddba726c98d
HandyGold75 <Ian.AnMaZo@gmail.com>
2025-01-12 15:34:36 +0100

    12-Jan-25

--- .server/build.sh
+++ .server/build.sh
@@ -14,6 +14,6 @@ build(){
     # ( env GOOS=windows GOARCH=386 CGO_ENABLED=1 CC=i686-w64-mingw32-gcc CXX=i686-w64-mingw32-g++ go build -o "$1.exe" . && echo -e "\033[32mBuild: $1.exe\033[0m" ) || { echo -e "\033[31mFailed: $1.exe\033[0m" ; return 1; }
 }
 
-file="hg75"
+file="HG75"
 update "$file" && build "$file"
 
