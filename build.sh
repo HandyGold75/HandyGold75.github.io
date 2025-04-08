7ad1adee32e4c2e7551b359ee916ac14227cf59a
HandyGold75 <Ian.AnMaZo@gmail.com>
2025-04-08 11:06:54 +0200

    08-Apr-25

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 update(){
-    # go get go@latest || { echo -e "\033[31mFailed: $1.*\033[0m" ; return 1; }
+    go get go@latest || { echo -e "\033[31mFailed: $1.*\033[0m" ; return 1; }
     go get -u || { echo -e "\033[31mFailed: $1.*\033[0m" ; return 1; }
     go mod tidy || { echo -e "\033[31mFailed: $1.*\033[0m" ; return 1; }
     for indirect in $(tail +3 go.mod | grep "// indirect" | awk '{if ($1 =="require") print $2; else print $1;}'); do go get -u "${indirect}"; done
