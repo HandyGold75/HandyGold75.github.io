345c44a7af5a1840f5ed3f82269e7b3cbd14df23
HandyGold75 <Ian.AnMaZo@gmail.com>
2025-07-23 19:42:13 +0200

    23-Jul-25

---
+++ server/srvs/Site.go
@@ -0,0 +1,53 @@
+package srvs
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/HandyGold75/GOLib/logger"
+)
+
+type (
+	SiteConfig struct{}
+
+	Site struct {
+		lgr  *logger.Logger
+		Pipe chan string
+		cfg  SiteConfig
+		exit bool
+	}
+)
+
+func NewSite(cfg SiteConfig) *Site {
+	lgr, _ := logger.NewRel("logs/site")
+	return &Site{cfg: cfg, Pipe: make(chan string), lgr: lgr}
+}
+
+func (s *Site) Run() {
+	s.lgr.Log("debug", "site", "starting")
+	go func() {
+		defer func() {
+			if rec := recover(); rec != nil {
+				s.exit = true
+				s.lgr.Log("error", "site", fmt.Sprintf("panic:%v; recovering", rec))
+			}
+			close(s.Pipe)
+		}()
+		s.loop()
+	}()
+	s.lgr.Log("medium", "site", "started")
+}
+
+func (s *Site) Stop() {
+	s.lgr.Log("debug", "site", "stopping")
+	s.exit = true
+	for range s.Pipe {
+	}
+	s.lgr.Log("medium", "site", "stopped")
+}
+
+func (s *Site) loop() {
+	for !s.exit {
+		time.Sleep(time.Second)
+	}
+}
